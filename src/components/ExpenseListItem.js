33894988d0033d86f0431bc9fc65ebcb74c58985
Matija Simin <marcussimon12082015@hotmail.com>
2020-01-18 11:40:57 -0800

    Setup formmatting for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { removeExpense } from '../actions/expenses';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
     <h3><Link to={`/edit/${id}`}>{description}</Link></h3>
-    <p>{amount} - {createdAt}</p>
+    <p>
+      {numeral(amount / 100).format('$0,0.00')}
+      -
+      {moment(createdAt).format('MMMM Do, YYYY')}
+    </p>
   </div>
 );
 
